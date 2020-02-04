b8c2c2220b708aae7735b91b6c0966b3c01e740f
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-04 22:45:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,7 +21,21 @@ end
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
-  binding.pry
-  result = {}
-  nil
+  director_total_gross = {}
+  row_index = 0
+  while row_index < nds.length do
+    director_name = nds[row_index][:name]
+    column_index = 0
+    while column_index < nds[row_index][:movies].length do
+      movie = nds[row_index][:movies][column_index]
+      if director_total_gross[director_name]
+        director_total_gross[director_name] += movie[:worldwide_gross]
+      else
+        director_total_gross[director_name] = movie[:worldwide_gross]
+      end
+    column_index += 1
+    end
+    row_index += 1
+  end
+  return director_total_gross
 end
